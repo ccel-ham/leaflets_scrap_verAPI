4024dbde384f7850c9caa497d3b694dadf1dfbd8
ccel-ham <90171055+ccel-ham@users.noreply.github.com>
2023-04-27 15:59:38 +0900

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-GOOGLE_URL = 'https://script.google.com/macros/s/AKfycbyZ0GdYBJ4GXefZR_C1iNtaMkA4NnktxdOzFgBHZuZugNHjBV9ETe4zoNS0w3CTLC1QWg/exec'
+GOOGLE_URL = 'https://script.google.com/macros/s/AKfycbzaqXGxMqcarGfnPCVS8RwSqsmNS2hpNOgj41G8l_7BupsEj0728Gf4x8kw-LWNODz8rA/exec'
 LINE_TOKEN = 'RGszAbYdsANHFRQ3b56ZLDh1YEJuQr6lWSAhxZtYxz2'
